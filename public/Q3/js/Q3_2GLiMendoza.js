43bbd246fac94a917c972a669c641045308911ab
SatyanaMendoza <b2029smsmendoza@pshs.edu.ph>
2026-01-12 21:28:53 +0800

    safety save and added css and js

---
+++ public/Q3/js/Q3_2GLiMendoza.js
@@ -0,0 +1,15 @@
+function confirmSubmit() {
+    return confirm("Are you sure you want to submit the form?");
+}
+
+function confirmReset() {
+    return confirm("Are you sure you want to reset the form?");
+}
+
+function checkEmpty(event) {
+    if (event.target.value.trim() === "") {
+        event.target.classList.add("missing");
+    } else {
+        event.target.classList.remove("missing");
+    }
+}
